dda0c4338f8363ac324ad88aaae2c0d7d95b056a
premhowli <howli.prem@gmail.com>
2018-07-05 20:04:16 +0530

    refactor /user/list

--- buymylooks/server/routes/api.js
+++ buymylooks/server/routes/api.js
@@ -13,32 +13,13 @@ const User = mongoose.model('User');
 
 
 
-// route to check token with postman.
-// using middleware to check for authorization header
-router.get('/verify', authService.checkTokenMW) /*(req, res) => {
-//     authService.verifyToken(req, res);
-// if (null === req.authData) {
-//     res.sendStatus(403);
-// } else {
-//     res.json(req.authData);
-// }
-}*/
+
 
 
 // List all the Users
-router.get('/users/list', authService.checkTokenMW, (req, res) => {
-    // authService.verifyToken(req, res);
-    if (null === req.authData) {
-    // res.sendStatus(403);
-} else {
-    User.find({}, function(err, users) {
-        if (err) throw err;
+router.get('/users/list', authService.checkTokenMW,(req,res)=> {
+    res.send("hello world");
 
-        // object of all the users
-        // console.log(users);
-        res.json(users);
-
-    });
 }
-});
+);
 module.exports = router;
